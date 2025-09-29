b48b6daf63bf6a8c60f255d673bbffd9fc7c0971
play0357 <2018190@miu.edu.mn>
2025-09-29 17:43:39 +0900

    서버 클라이언트 불러오기 파일 수정

--- src/client/client.js
+++ src/client/client.js
@@ -1,8 +1,13 @@
 //클라이언트 파일을 따로 만들어서 불러다 쓰자
 
+import { createHttpLink } from "@apollo/client";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-export const client = new ApolloClient({
+const link = createHttpLink({
     uri: 'http://localhost:4000/',
+})
+
+export const client = new ApolloClient({
+    link,
     cache: new InMemoryCache() // 데이터 캐싱 
 })
